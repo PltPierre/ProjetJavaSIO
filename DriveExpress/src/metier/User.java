05298ef6bfd06c1b9f691230530f43238d4a5a28
Pierre Pairault <51343963+PltPierre@users.noreply.github.com>
2020-12-14 20:26:12 +0100

    Update User.java

--- DriveExpress/src/metier/User.java
+++ DriveExpress/src/metier/User.java
@@ -9,6 +9,7 @@ public class User {
 	private int CP;
 	private int NumTel;
 	private String userMail;
+	private String userPassword
 	
 	public User(int iDUser, String nomUser, String prenomUser, String adresse, String ville, int cP, int numTel, String userMail) {
 		this.IDUser = iDUser;
