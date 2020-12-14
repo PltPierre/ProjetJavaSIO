46b19120a3375778397742256f17c6b968248d51
Pierre Pairault <pairaulltpierre@gmail.com>
2020-12-14 20:06:29 +0100

    User class created

---
+++ DriveExpress/src/metier/User.java
@@ -0,0 +1,25 @@
+package metier;
+
+public class User {
+	private int IDUser;
+	private String NomUser;
+	private String PrenomUser;
+	private String adresse;
+	private String Ville;
+	private int CP;
+	private int NumTel;
+	private String userMail;
+	
+	public User(int iDUser, String nomUser, String prenomUser, String adresse, String ville, int cP, int numTel, String userMail) {
+		this.IDUser = iDUser;
+		this.NomUser = nomUser;
+		this.PrenomUser = prenomUser;
+		this.adresse = adresse;
+		this.Ville = ville;
+		this.CP = cP;
+		this.NumTel = numTel;
+		this.userMail = userMail;
+	}
+	
+	
+}
