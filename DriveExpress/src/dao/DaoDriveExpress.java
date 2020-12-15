096e9014ad818444e54633531277ed2e3aaf6b33
Pierre Pairault <pairaulltpierre@gmail.com>
2020-12-15 19:18:20 +0100

    conf.properties ajouté pour pas avoir le login, mdp et url visible

--- DriveExpress/src/dao/DaoDriveExpress.java
+++ DriveExpress/src/dao/DaoDriveExpress.java
@@ -1,33 +1,53 @@
 package dao;
 
+import java.io.FileInputStream;
 import java.sql.*;
+import java.util.Properties;
+import java.util.TreeMap;
 
 public class DaoDriveExpress 
 {
-	private static final String nomPilote = "org.mariadb.jdbc.Driver";
-	private static final String url = "jdbc:mysql://bch5pl5adcyod7rdbeyl-mysql.services.clever-cloud.com:3306:bch5pl5adcyod7rdbeyl";
-	private static final String user = "uf5o6ck7ntrtuzw7";
-	private static final String password = "vIt8MTnd0AfFn6lNTGaJ";
+	private static final TreeMap<String, String> properties = DaoDriveExpress.GetDatabaseProperties();
 	
 	public static Connection SQLConnection() {
 		
 		Connection connect = null;
 		
 		try {
-			Class.forName(nomPilote);
+			Class.forName(properties.get("driver"));
 		} catch (ClassNotFoundException e1) {
 			System.out.println("pb driver");
 			e1.printStackTrace();
 		}
 		
 		try {
-			connect = DriverManager.getConnection(DaoDriveExpress.url,DaoDriveExpress.user,DaoDriveExpress.password);
+			connect = DriverManager.getConnection(properties.get("url"),properties.get("login"),properties.get("password"));
 		} catch (SQLException e) {
 			System.out.println("pb lors de la connection a la base de données");
 			e.printStackTrace();
 		}
 		
 		return connect;
+	}
+	
+	private static TreeMap<String, String> GetDatabaseProperties()
+	{
+		TreeMap<String, String> properties = new TreeMap<String, String>();
+		
+		Properties props = new Properties();
+		try{
+			FileInputStream fis = new FileInputStream("conf.properties");
+			props.load(fis);
+		}catch(Exception e){
+			e.getStackTrace();
+		}
+		
+		properties.put("driver", props.getProperty("jdbc.driver"));
+		properties.put("url", props.getProperty("jdbc.url"));
+		properties.put("login", props.getProperty("jdbc.login"));
+		properties.put("url", props.getProperty("jdbc.password"));
+		
+		return properties;
 	}
 	
 }
