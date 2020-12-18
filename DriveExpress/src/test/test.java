e45454eb3160cbbc2270deccacda52890669be9c
Pierre Pairault <pairaulltpierre@gmail.com>
2020-12-18 22:46:35 +0100

    formating code

--- DriveExpress/src/test/test.java
+++ DriveExpress/src/test/test.java
@@ -6,24 +6,24 @@ import dao.DaoDriveExpress;
 
 public class test {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Connection connect = DaoDriveExpress.SQLConnection();
-		
-		try {
-			Statement stLienBD = connect.createStatement();
-			String req = "SELECT * FROM USERS";
-			ResultSet resultat = stLienBD.executeQuery(req);
-			while(resultat.next()) {
-				System.out.println(resultat.getString(1) + "\t" + resultat.getString(2) + "\t" + resultat.getString(3));
-			}
-			
-			if(connect != null) {
-				connect.close();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	Connection connect = DaoDriveExpress.SQLConnection();
+
+	try {
+	    Statement stLienBD = connect.createStatement();
+	    String req = "SELECT * FROM USERS";
+	    ResultSet resultat = stLienBD.executeQuery(req);
+	    while (resultat.next()) {
+		System.out.println(resultat.getString(1) + "\t" + resultat.getString(2) + "\t" + resultat.getString(3));
+	    }
+
+	    if (connect != null) {
+		connect.close();
+	    }
+	} catch (SQLException e) {
+	    e.printStackTrace();
 	}
+    }
 
 }
