5ea477536e0524a1372bc13e7ec908d4ff57bf2c
Pierre Pairault <pairaulltpierre@gmail.com>
2020-12-18 02:28:54 +0100

    Creation de l'instance la MainPage

--- DriveExpress/src/main/Main.java
+++ DriveExpress/src/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import ihm.MainPage;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-
+		MainPage mp = new MainPage();
+		mp.setUndecorated(true);
+		mp.setVisible(true);
 	}
 
 }
