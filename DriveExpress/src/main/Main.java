7dbe09f3ff60a651e6a564a663db268a646469e8
Pierre Pairault <pairaulltpierre@gmail.com>
2020-12-18 23:22:35 +0100

    ajout parametre Connection connection a l'instantiation de la fenetre

--- DriveExpress/src/main/Main.java
+++ DriveExpress/src/main/Main.java
@@ -1,11 +1,12 @@
 package main;
 
+import dao.DaoDriveExpress;
 import ihm.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	MainPage mp = new MainPage();
+	MainPage mp = new MainPage(DaoDriveExpress.SQLConnection());
 	mp.setUndecorated(true);
 	mp.setVisible(true);
     }
